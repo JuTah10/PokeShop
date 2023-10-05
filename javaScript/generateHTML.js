648b70019ed5a0453d6ff3ea9fb6afd8b3d0d99b
Vu Nguyen <vuthanhnguyen201004@gmail.com>
2023-10-05 14:14:16 -0400

    get data from the data provided and generate it to HTML

--- javaScript/generateHTML.js
+++ javaScript/generateHTML.js
@@ -1,22 +1,3 @@
-const products = [{
-    image:  "../image/pikachu.webp",
-    name:   "Pokemon Plush Doll Oversize Cute Anime Pikachu",
-    rating: {
-        stars:  4.5,
-        count:  79
-    },
-    price:  1090
-},
-{
-    image:  "../image/fox-poke.jpg",
-    name:   "Flareon Poke Plush",
-    rating: {
-        stars:  4,
-        count:  127
-    },
-    price:  2095
-}]
-
 let productsHTML = ``;
 
 products.forEach((product)=>{
---
+++ product-data/products.js
@@ -0,0 +1,27 @@
+const products = [{
+    image:  "../image/pikachu.webp",
+    name:   "Pokemon Plush Doll Oversize Cute Anime Pikachu",
+    rating: {
+        stars:  4.5,
+        count:  79
+    },
+    price:  1090
+},
+{
+    image:  "../image/fox-poke.jpg",
+    name:   "Flareon Poke Plush",
+    rating: {
+        stars:  4,
+        count:  127
+    },
+    price:  2095
+},
+{
+    image:  "../image/mew-poke.png",
+    name:   "Mew Plush Pokemon fit",
+    rating: {
+        stars:  4.5,
+        count:  56
+    },
+    price:  799
+}]
