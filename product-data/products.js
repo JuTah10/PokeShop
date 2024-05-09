2e3384d0dc346979d35d3b03a49d5635d2c6914d
Vu Nguyen <vuthanhnguyen201004@gmail.com>
2024-05-09 00:44:14 -0400

    first commit

--- product-data/products.js
+++ product-data/products.js
@@ -7,7 +7,7 @@ export const products = [{
         count:  79
     },
     price:  1090
-},
+}, 
 {
     id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     image:  "../image/fox-poke.jpg",
@@ -27,5 +27,16 @@ export const products = [{
         count:  56
     },
     price:  799
+},
+{
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2",
+    image:  "../image/whalePoke.png",
+    name:   "Cartoon Pokemon Plush Stuffed Toy",
+    rating: {
+        stars:  4.5,
+        count:  56
+    },
+    price:  5000
 }]
+
 
